9b1eb4cbcb0a0b86f6f9d2b2d5cd7a3dcf5b70e2
devdeer-kevin <kevin.heyland@devdeer.com>
2023-05-18 16:02:28 +0200

    added simple layout and style

--- auto-backlog-2000/src/pages/index.tsx
+++ auto-backlog-2000/src/pages/index.tsx
@@ -1,15 +1,22 @@
 export default function Home() {
     return (
         <main className="w-full min-h-screen items-center font-mono bg-indigo-50 p-24">
-            <div className="grid grid-flow-row gap-4 auto-rows-max w-full">
+            <div className="grid grid-flow-row gap-8 auto-rows-max w-full">
                 <div className="w-full">
                     <h1 className="text-4xl font-bold text-slate-600">Auto Backlog 2000</h1>
                 </div>
                 <div className="w-full grid">
+                    <label className="label font-semibold text-slate-600">Requirement Engineering Notes</label>
                     <textarea
-                        className="textarea w-full h-96 p-4 rounded-ld shadow-md resize-non focus:outline-none"
+                        className="textarea w-full h-96 p-4 rounded-md shadow-md resize-non focus:outline-none"
                         placeholder="Enter your requirement engineering notes here..."
                     />
+                </div>
+                <div className="w-full grid">
+                    <label className="label font-semibold text-slate-600">Result</label>
+                    <div className="w-full h-96 text-sm pl-4 pt-6 rounded-md shadow-md bg-white overflow-auto">
+                        <p>API response</p>
+                    </div>
                 </div>
             </div>
         </main>
